30f31dd1f9d6810709ef5a5efeadbb98377848f1
Aleksandra Łukaszewicz <aleksandra.lukaszewicz@outlook.com>
2020-04-21 17:41:27 +0200

    Initial commit

---
+++ src/test/java/binarek/robio/RobioApplicationTests.java
@@ -0,0 +1,13 @@
+package binarek.robio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
